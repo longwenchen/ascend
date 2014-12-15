6063d3f3be1766a85f8350826efff62d7651cec3
Longwen Chen <chenlongwen@gmail.com>
2014-12-15 16:57:17 +0800

    change input form button style

--- lwc/settings.py
+++ lwc/settings.py
@@ -84,13 +84,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-		    os.path.join(BASE_DIR, 'static'),
-		    )
+    os.path.join(BASE_DIR, 'static'),
+)
 
 TEMPLATE_DIRS = (
-		    os.path.join(BASE_DIR, 'templates'),
-		    )
+    os.path.join(BASE_DIR, 'templates'),
+)
 
 MEDIA_ROOT = (
-		    os.path.join(BASE_DIR, 'media')
-		    )
+    os.path.join(BASE_DIR, 'media')
+)
